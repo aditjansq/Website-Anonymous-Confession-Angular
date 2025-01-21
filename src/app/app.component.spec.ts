b31eff51a6b90d714ebf996740eb4a1268d3e922
adit <aditjansa_2226240046@mhs.mdp.ac.id>
2025-01-21 19:30:05 +0700

    .

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'todolist-site' title`, () => {
+  it(`should have the 'confess-site' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('todolist-site');
+    expect(app.title).toEqual('confess-site');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, todolist-site');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, confess-site');
   });
 });
